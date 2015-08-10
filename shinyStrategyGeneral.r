746109b2ddabe8d6bff387be35ae94013a17fea1
ImennoOn <romadanovskiy@MiRo-Pro.local>
2015-08-10 20:29:40 +0300

    First commit

---
+++ shinyStrategyGeneral.r
@@ -0,0 +1,11 @@
+###################################
+# Launch Shiny App
+# 
+# thertrader@gmail.com - June 2014
+###################################
+library(PerformanceAnalytics)
+library(shiny)
+library(xts)
+
+runApp("~/Documents/GitHub/FinMarketsAnalysisR/ShinyMarkets", options(shiny.error=traceback))
+
